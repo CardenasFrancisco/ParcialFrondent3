ea7a5a1349eedf8e366c4290f4b85226bc03323e
CardenasFrancisco <francocardenas848@gmail.com>
2022-09-02 22:47:30 -0500

    first commit

---
+++ entregable-frontend-3-junio22-franciscoCardenas/my-app/src/components/Item.js
@@ -0,0 +1,44 @@
+// El componente Item no tiene componentes hijos.
+// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
+// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
+// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
+// Maqueta de Item:
+//    h3
+//    p
+//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
+//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+
+import { useState } from "react";
+
+
+export default function Item({nombre,descripcion,stock,tarjetas}) {
+  const [currentStock, setCurrentStock] = useState(stock);
+  const newStock = () => {
+    setCurrentStock(currentStock - 1);
+  };
+  
+  const addToCart = () => {
+    if (currentStock > 0) {
+      newStock();
+      tarjetas();
+    } else {
+      setCurrentStock(0);
+    }
+  };
+  let availableStock = currentStock > 0 ? true : false;
+  return (
+    <div className="producto">
+      <h3>{nombre}</h3>
+      <p>{descripcion}</p>
+      <h5>
+        En stock:{" "}
+        <span className={availableStock ? "withStock" : "withoutStock"}>
+          {availableStock ? currentStock : "Agotado"}
+        </span>
+      </h5>
+      <button onClick={addToCart} disabled={availableStock ? false : true}>
+        {availableStock ? "COMRAR" : "SIN STOCK"}
+      </button>
+    </div>
+  );
+}
